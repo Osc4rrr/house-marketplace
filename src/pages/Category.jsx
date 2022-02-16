a0f8e0d761629850fd935fe8d90a23d81b0158da
Oscar Diaz <os.diazl@alumnos.duoc.cl>
2022-02-16 19:13:45 -0300

    pages added
    Explore page, fetch listing, listing item,
    offers page, listing page, create listing page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,7 +14,7 @@ import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
 
-const Offers = () => {
+const Category = () => {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -29,7 +29,7 @@ const Offers = () => {
         //Create a query
         const q = query(
           listingsRef,
-          where("offer", "==", true),
+          where("type", "==", params.categoryName),
           orderBy("timestamps", "desc"),
           limit(10)
         );
@@ -53,12 +53,16 @@ const Offers = () => {
     };
 
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
 
   return (
     <div className="category">
       <header>
-        <p className="pageHeader">Offers</p>
+        <p className="pageHeader">
+          {params.categoryName == "rent"
+            ? "Places For Rent"
+            : "Places For Sell"}
+        </p>
       </header>
 
       {loading ? (
@@ -78,10 +82,10 @@ const Offers = () => {
           </main>
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>No listings for {params.categoryName}</p>
       )}
     </div>
   );
 };
 
-export default Offers;
+export default Category;
